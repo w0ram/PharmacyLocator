3ffdef7b2bfcf55f72848d06928f75e6124c2444
w0ram <55700217+w0ram@users.noreply.github.com>
2023-06-03 14:09:21 +0100

    Update UserController.java

--- src/main/java/gestion/packages/controllers/UserController.java
+++ src/main/java/gestion/packages/controllers/UserController.java
@@ -19,7 +19,7 @@ import gestion.packages.services.UserService;
 
 @RestController
 @RequestMapping("api/user")
-@CrossOrigin()
+@CrossOrigin
 public class UserController {
 	@Autowired
 	private UserService userServ;
