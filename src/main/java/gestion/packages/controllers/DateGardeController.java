9df01fec25481a2c0876ac4999e1b6ec66a6fda9
w0ram <55700217+w0ram@users.noreply.github.com>
2023-06-03 14:05:16 +0100

    Update DateGardeController.java

--- src/main/java/gestion/packages/controllers/DateGardeController.java
+++ src/main/java/gestion/packages/controllers/DateGardeController.java
@@ -20,7 +20,7 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("api/GardePharmacie")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin()
 public class DateGardeController {
     @Autowired
     private DateGardeService dateGardeService;
