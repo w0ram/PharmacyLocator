78f4a1bf5d05520e909142ca4e94e28285006f3a
w0ram <55700217+w0ram@users.noreply.github.com>
2023-06-03 14:08:08 +0100

    Update ZoneController.java

--- src/main/java/gestion/packages/controllers/ZoneController.java
+++ src/main/java/gestion/packages/controllers/ZoneController.java
@@ -24,7 +24,7 @@ import gestion.packages.services.ZoneService;
 
 @RestController
 @RequestMapping("api/zone")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 public class ZoneController {
 	@Autowired
 	private ZoneService zoneServ;
